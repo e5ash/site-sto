f3d4209db1dfdd7e8711e3f0b9757e53c3b7976d
Oleg Frey <olfrey@ya.ru>
2018-09-14 16:51:50 +0300

    full

--- src/template/scripts/common.js
+++ src/template/scripts/common.js
@@ -31,7 +31,7 @@ $(document).ready(function($) {
 
 
 	// nav items arrow
-	$('.nav-drop__list ul').each(function(index, el) {
+	$('.nav-drop__list ul, .mobile-nav__list ul').each(function(index, el) {
 		var li = $(this).parents('li');
 
 		li.addClass('has-subnav');
@@ -43,7 +43,9 @@ $(document).ready(function($) {
 	var slider = $('.slider__list');
 	slider.slick({
 		dots: true,
-		fade: true
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 5000
 	})
 
 
@@ -68,4 +70,143 @@ $(document).ready(function($) {
 	});
 
 
+
+
+	// stars
+	$('.star').click(function(event) {
+		var starsPrev = $(this).prevAll('.star'),
+				starsNext = $(this).nextAll('.star'),
+				starText = $(this).find('input').attr('data-text');
+
+		starsPrev.addClass('star_o');
+		$(this).addClass('star_o')
+		starsNext.removeClass('star_o');
+
+		// starsText.html(starText);
+	});
+
+
+	// elements form
+	function valueElementForm(nameElement, nameBlock) {
+		var newNameElement = '.' + nameElement;
+			element = $(newNameElement);
+		element.each(function(index, el) {
+			var elementInput = $(this).find($(nameBlock)),
+				elementLabel = $(this).find($('label')),
+				elementValue = index + 1;
+			elementInput.attr('id', nameElement + '-' + elementValue);
+			elementLabel.attr('for', nameElement + '-' + elementValue);
+		});
+		
+	}
+	valueElementForm('input', 'input');
+	valueElementForm('textarea', 'textarea');
+	valueElementForm('file', 'input');
+	valueElementForm('policy', 'input');
+	
+
+	// mobile nav
+
+	var mobileNav = $('.mobile-nav'),
+			mobileNavClass = 'mobile-nav_toggle',
+			mobileNavClose = $('.mobile-nav__close'),
+			mobileNavReturn = $('.mobile-nav__return'),
+			mobileNavReturnClass = 'mobile-nav__return_show',
+			mobileNavTitle = $('.mobile-nav__title'),
+			humburger = $('.hum');
+
+
+	humburger.click(function() {
+		mobileNav.addClass(mobileNavClass);
+	});
+
+	mobileNavClose.click(function(event) {
+		mobileNav.removeClass(mobileNavClass);
+	});
+
+
+	$('.mobile-nav__list a').click(function(event) {
+		var href = $(this).attr('href'),
+				text = $(this).text();
+
+		if (href == '#') {
+			event.preventDefault();
+			mobileNavTitle.html(text);
+			mobileNavReturn.addClass(mobileNavReturnClass);
+		}
+		var ul = $(this).parents('ul'),
+				ulSub = $(this).siblings('ul');
+		ul.addClass('minus');
+		ulSub.addClass('visible');
+	});
+
+	mobileNavReturn.click(function(event) {
+		var ul = mobileNav.find('ul.minus > li > ul.visible'),
+				ulParent = ul.parents('ul');
+
+		if (ulParent.hasClass('visible')) {
+			ulParent = ul.parents('ul.minus.visible');
+			ulParent.addClass('visible');
+		} else{
+			ul.removeClass('visible');
+		}
+		if (ulParent.hasClass('main-ul')) {
+			mobileNavTitle.html('');
+			mobileNavReturn.removeClass(mobileNavReturnClass);
+		} else{
+			var link = ulParent.prev('a').text();
+			mobileNavTitle.html(link);
+		}
+		
+
+		ulParent.removeClass('minus');	
+		
+	});
+
+	// footer nav
+
+	$('.footer__title').click(function() {
+		var nav = $(this).next('.footer__nav');
+		$(this).toggleClass('footer__title_toggle');
+		nav.slideToggle(300);
+	});
+
+
+
+
+	// sliders mobile
+
+  $advantages = $('.advantages__list');
+  $photos = $('.photos__list');
+  $certificates = $('.certificates__list');
+
+  var settings_one = {
+    dots: true,
+    arrows: false
+  }
+
+  var settings_two = {
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows: false
+  }
+
+  slick_on_mobile( $advantages, settings_one);
+  slick_on_mobile( $photos, settings_one);
+  slick_on_mobile( $certificates, settings_two);
+
+  function slick_on_mobile(slider, settings){
+    $(window).on('load resize', function() {
+      if ($(window).width() > 767) {
+        if (slider.hasClass('slick-initialized')) {
+          slider.slick('unslick');
+        }
+        return
+      }
+      if (!slider.hasClass('slick-initialized')) {
+        return slider.slick(settings);
+      }
+    });
+  };
 });
